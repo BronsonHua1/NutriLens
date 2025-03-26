cb465ae7092cf17622b4a6cb8f2d15195b3f7e18
dross01 <David.Ross01@student.csulb.edu>
2025-03-25 17:36:12 -0700

    login and registration synced with Firebase Authentication and Realtime Database

--- lib/pages/change_email.dart
+++ lib/pages/change_email.dart
@@ -2,7 +2,6 @@ import '../components/button_fab.dart';
 import '../components/text_field_fab.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 class ChangeEmail extends StatelessWidget {
   final emailController = TextEditingController();
